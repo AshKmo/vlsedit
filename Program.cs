c392e849a93b7301f33675a7320c5bb17df9a383
AshKmo <ashkmo33@gmail.com>
2025-10-10 16:07:42 +1100

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+using SplashKitSDK;
+
+namespace VLSEdit
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Window window = new Window("VLSEdit", 1080, 720);
+        }
+    }
+}
